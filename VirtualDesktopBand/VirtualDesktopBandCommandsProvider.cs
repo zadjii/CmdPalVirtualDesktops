4a0e3fb21374aa24c6f42ce448c7330b0b2a558f
Mike Griese <migrie@microsoft.com>
2025-12-03 12:59:36 -0600

    the whole thing

--- VirtualDesktopBand/VirtualDesktopBandCommandsProvider.cs
+++ VirtualDesktopBand/VirtualDesktopBandCommandsProvider.cs
@@ -4,6 +4,13 @@
 
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsDesktop;
 
 namespace VirtualDesktopBand;
 
@@ -16,7 +23,7 @@ public partial class VirtualDesktopBandCommandsProvider : CommandProvider
         DisplayName = "Deskband for virtual desktops";
         Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
         _commands = [
-            new CommandItem(new VirtualDesktopBandPage()) { Title = DisplayName },
+            new CommandItem(new VirtualDesktopsListPage()) { Title = DisplayName },
         ];
     }
 
@@ -26,3 +33,118 @@ public partial class VirtualDesktopBandCommandsProvider : CommandProvider
     }
 
 }
+
+
+public partial class VirtualDesktopsListPage : ListPage
+{
+    //private DispatcherQueue _queue = DispatcherQueue.GetForCurrentThread();
+    TaskScheduler _scheduler;//  = TaskScheduler.Current;// TaskScheduler.FromCurrentSynchronizationContext();
+
+    public override string Id => "com.zadjii.virtualDesktops.band";
+
+    public static readonly IconInfo CheckboxEmptyIcon = new("\uE739");
+    public static readonly IconInfo CheckboxFillIcon = new("\uE73B");
+    public static readonly IconInfo ToggleFilledIcon = new("\uEC11");
+    public static readonly IconInfo StatusCircleIcon = new("\uEA81");
+    public static readonly IconInfo CircleFillBadge12Icon = new("\uEDB0");
+
+    private VirtualDesktop[] _desktops;
+
+    public VirtualDesktopsListPage()
+    {
+        _scheduler = TaskScheduler.Current;
+
+        VirtualDesktop.CurrentChanged += (_, args) => UpdateDesktopsOffUiThread();
+        VirtualDesktop.Created += (_, desktop) => UpdateDesktopsOffUiThread();
+
+        _desktops = VirtualDesktop.GetDesktops();
+
+    }
+
+    public override IListItem[] GetItems()
+    {
+        VirtualDesktop[] desktops = [];
+
+
+
+        //Logger.LogDebug("VirtualDesktopsListPage.GetItems");
+        //VirtualDesktop[] desktops = VirtualDesktop.GetDesktops();
+        List<IListItem> items = new(_desktops.Length);
+        DebugPrint($"Current desktop is {VirtualDesktop.Current}");
+        foreach (VirtualDesktop desktop in _desktops)
+        {
+            items.Add(DesktopToItem(desktop));
+
+        }
+        DebugPrint(string.Join(',', items.Select(i => i.Command.ToString())));
+        return items.ToArray();
+    }
+
+    private void UpdateDesktopsOffUiThread()
+    {
+        //Task.Factory.StartNew(UpdateDesktopsOnUiThread, )
+        //Task.Run(UpdateDesktopsOnUiThread, _scheduler);
+        //_queue.TryEnqueue(UpdateDesktopsOnUiThread);
+        Task.Factory.StartNew(UpdateDesktopsOnUiThread,
+            CancellationToken.None,
+            TaskCreationOptions.None,
+            _scheduler);
+    }
+
+    private void UpdateDesktopsOnUiThread()
+    {
+        _desktops = VirtualDesktop.GetDesktops();
+        RaiseItemsChanged();
+    }
+
+    private static IListItem DesktopToItem(VirtualDesktop desktop)
+    {
+        bool isCurrent = desktop == VirtualDesktop.Current;
+        if (isCurrent)
+        {
+            DebugPrint($"    * I ({desktop.ToString()}) am current");
+        }
+        else
+        {
+            DebugPrint($"    - I am NOT current");
+        }
+
+        return new ListItem(new SwitchToDesktopCommand(desktop, isCurrent))
+        {
+            // Icon = isCurrent ? CheckboxFillIcon : CheckboxEmptyIcon
+            Icon = isCurrent ? ToggleFilledIcon : CircleFillBadge12Icon
+            //Icon = isCurrent ? new(desktop.WallpaperPath) : CircleFillBadge12Icon
+        };
+    }
+
+    private sealed partial class SwitchToDesktopCommand(VirtualDesktop desktop, bool isCurrent) : InvokableCommand
+    {
+        public VirtualDesktop Desktop { get; } = desktop;
+        public override string Name => string.Empty;
+        internal bool IsCurrent { get; init; } = isCurrent;
+        public override string ToString()
+        {
+            return $"{(IsCurrent ? "*" : string.Empty)}{Desktop.ToString()}";
+        }
+        public override ICommandResult Invoke()
+        {
+            try
+            {
+                DebugPrint($"Switching to '{Desktop.ToString()}'");
+                desktop.Switch();
+                DebugPrint($"...done");
+            }
+            catch (Exception e)
+            {
+                DebugPrint($"SwitchToDesktopCommand invoke\n{e.Message}\n{e.StackTrace}");
+            }
+            return CommandResult.KeepOpen();
+        }
+    }
+
+    private static void DebugPrint(string? s)
+    {
+        Debug.WriteLine(s);
+    }
+}
+
